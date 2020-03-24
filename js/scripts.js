5ae8e3f9cadbfd69970b6d57be654b5e46e4e86b
mai cook <mailiecook@yahoo.com>
2020-03-24 16:16:28 -0700

    debug scripts.js, add ids user, and webpage in index.html lines 31 and 38

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".card-text").fadeToggle();
-});
+  });
     $("button#hello").click(function() {
-      $("ul").prepend("<li>Hello!</li>");
+      $("ul#user").prepend("<li>Hello!</li>");
+      $("ul#webpage").prepend("<li>Why hello there!</li>");
     });
   
     $("button#goodbye").click(function() {
-      $("ul").prepend("<li>Goodbye!</li>");
+      $("ul#user").prepend("<li>Goodbye!</li>");
+      $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
     });
   
     $("button#stop").click(function() {
-      $("ul").prepend("<li>Stop copying me!</li>");
+      $("ul#user").prepend("<li>Stop copying me!</li>");
+      $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
     });
   });
 
