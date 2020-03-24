1d7c20fadf9c54c56f8c4e7c205939eb90b7a07a
mai cook <mailiecook@yahoo.com>
2020-03-24 16:00:09 -0700

    add content in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $(".card-text").fadeToggle();
-} );
 });
+    $("button#hello").click(function() {
+      $("ul").prepend("<li>Hello!</li>");
+    });
+  
+    $("button#goodbye").click(function() {
+      $("ul").prepend("<li>Goodbye!</li>");
+    });
+  
+    $("button#stop").click(function() {
+      $("ul").prepend("<li>Stop copying me!</li>");
+    });
+  });
+
+
